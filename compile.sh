44008ed64708e2f289be60f3278f976b43b3929e
David <david-adeniji@hotmail.co.uk>
2024-01-29 20:48:31 +0000

    Cloudformation setup

---
+++ compile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set variables
+BUCKET_NAME="xxxx"
+STACK_NAME="render-reviver-stack"
+FUNCTION_NAME="render-reviver"
+ZIP_FILE="RenderReviver.zip"
+TEMPLATE_FILE="template.yaml"
+
+# Generate requirements.txt from Pipfile.lock
+pipenv lock --requirements > requirements.txt
+
+# Install dependencies into the package directory
+pipenv run pip install -r requirements.txt --target ./package
+
+# Zip your package directory that contains your function and dependencies
+cd package
+zip -r ../$ZIP_FILE .
+cd ..
+
+# Upload the zip file to S3
+aws s3 cp $ZIP_FILE s3://$BUCKET_NAME
+
+# Try to update the CloudFormation stack
+aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://$TEMPLATE_FILE --capabilities CAPABILITY_IAM
+
+# Capture the exit status of the CloudFormation update command
+CFN_UPDATE_EXIT_STATUS=$?
+
+# If the CloudFormation update failed (non-zero exit status), then try updating the function code directly
+if [ $CFN_UPDATE_EXIT_STATUS -ne 0 ]; then
+    echo "CloudFormation stack update failed, trying to update the Lambda function code directly..."
+    aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket $BUCKET_NAME --s3-key $ZIP_FILE
+fi
